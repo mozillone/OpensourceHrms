802dddfc2a7414d7eaae5266c680b673855462c6
Mozillone <mozillone@gmail.com>
2018-05-01 19:04:51 +0530

    Changes made to Hrms in Testbase class

---
+++ src/main/java/com/OpenHrms/qa/base/TestBase.java
@@ -0,0 +1,58 @@
+package com.OpenHrms.qa.base;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+
+public class TestBase {
+	
+	static WebDriver driver;
+	public static Properties prop;
+	
+	
+	public TestBase(){
+		
+		try {
+			prop = new Properties();
+			FileInputStream ip = new FileInputStream("C:\\Users\\admin\\workspace\\eclipse workspace\\OpensrcHrms\\src\\main\\java\\com\\OpenHrms\\qa\\config\\config.properties");
+			prop.load(ip);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void initialization(){
+		String browserName = prop.getProperty("browser");
+		
+		if(browserName.equals("chrome")){
+			System.setProperty("webdriver.chrome.driver", "D:\\Automation Data\\chromedriver\\chromedriver.exe");
+			 driver = new ChromeDriver();
+			 
+		}
+		else if(browserName.equals("firefox")){
+			System.setProperty("webdriver.gecko.driver", "D:\\Automation Data\\geckodriver.exe");
+			 driver = new FirefoxDriver();
+			
+	}
+		
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.get(prop.getProperty("Url"));
+	
+	}
+		
+}
+
+
---
+++ src/main/java/com/OpenHrms/qa/pages/LoginPage.java
@@ -0,0 +1,5 @@
+package com.OpenHrms.qa.pages;
+
+public class LoginPage {
+
+}
